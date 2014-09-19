760ca2fe2392a93c59c47ae5b5be29758422e3f9
Dmitry Samsonau <demn_@tut.by>
2014-09-19 16:37:10 +0300

    Adding tumblr photos from posts

--- TumblrExample/Controllers/HomeController.cs
+++ TumblrExample/Controllers/HomeController.cs
@@ -1,25 +1,34 @@
 ﻿using System.Web.Mvc;
 using DontPanic.TumblrSharp;
 using DontPanic.TumblrSharp.Client;
-using DontPanic.TumblrSharp.OAuth;
 
 namespace TumblrExample.Controllers {
     public class HomeController : Controller {
+        //[HttpGet]
+        //public ActionResult Index(string oauth_token, string oauth_verifier) {
+
+        //    var tc = new TumblrClient(new HmacSha1HashProvider(), "z35mVaZpxg4JC7IdJ1KB1GHYOqYXfwwnv0cIOlHafuMUZ7Tlat",
+        //        "gn3rVOlvrYVjLZP4tYnxb1FXYNiFGFZt3twWotz84VeYb9oWbj", new Token("AyfIkgc50nE5FrRMw12tcvwkzBAuPTYz9X02Tz7JMK3wfSklGn", "il1gFmryYosORAoiuHiYqYco10gPXt0Hau6f717ENzFImqLhuH"));
+
+        //    var qq = tc.GetPostsAsync("samsonau");
+        //    qq.Wait();
+
+        //    return View();
+        //}
+
         [HttpGet]
-        public ActionResult Index(string oauth_token, string oauth_verifier) {
+        public ActionResult Index() {
 
-            //var tc = new TumblrClient(new HmacSha1HashProvider(), "z35mVaZpxg4JC7IdJ1KB1GHYOqYXfwwnv0cIOlHafuMUZ7Tlat",
-            //    "gn3rVOlvrYVjLZP4tYnxb1FXYNiFGFZt3twWotz84VeYb9oWbj", oauth_token);
-            //var q = tc.GetBlogInfoAsync("samsonau");
-            //q.Wait();
+            var tc = new TumblrClient(new HmacSha1HashProvider(), "z35mVaZpxg4JC7IdJ1KB1GHYOqYXfwwnv0cIOlHafuMUZ7Tlat",
+                "gn3rVOlvrYVjLZP4tYnxb1FXYNiFGFZt3twWotz84VeYb9oWbj");
 
-            //var qq = tc.GetDashboardPostsAsync();
-            //qq.Wait();
-            //Task<BlogInfo> blogInfo = GetBlockInfo(tc);
-            //var qq = blogInfo.Result;
+            var qq = tc.GetPostsAsync("samsonau");
+            qq.Wait();
 
-            return View();
+            var q = qq.Result;
+
+            Photo[] photoSet = ((PhotoPost)(q.Result[0])).PhotoSet;
+            return View(photoSet);
         }
-
     }
 }
